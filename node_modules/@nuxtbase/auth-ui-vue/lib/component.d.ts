41557507ba6aff24373f0f791f725b72749dceb2
vinhln <lengocvinh.729@gmail.com>
2024-04-30 16:28:15 +0700

    feat: add supabase setup

---
+++ node_modules/@nuxtbase/auth-ui-vue/lib/component.d.ts
@@ -0,0 +1,5 @@
+export { default as Auth } from './auth/Auth.vue';
+export { default as ForgottenPassword } from './auth/ForgottenPassword.vue';
+export { default as UpdatePassword } from './auth/UpdatePassword.vue';
+export { default as MagicLink } from './auth/MagicLink.vue';
+export { default as UserContextProvider, useSupabaseUser } from './auth/UserContextProvider';
